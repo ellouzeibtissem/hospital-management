a27b6a1a13050404344f3ecb4b37744d5fbde39c
ellouzeibtissem <ibtissem.ellouze@enetcom.u-sfax.tn>
2024-06-21 11:25:48 +0100

    sql constraints in odoo

--- hospital/models/patient_tag.py
+++ hospital/models/patient_tag.py
@@ -9,3 +9,7 @@ class PatientTag(models.Model):
     active = fields.Boolean(string='Active', default=True)
     color = fields.Integer(string="Color")
     color_2 = fields.Char(string="Color 2")
+    sequence = fields.Integer(string='Sequence')
+    _sql_constraints = [
+        ('unique_tag_name', 'unique (name,active)', "Tag name already exists!"),
+        ('check_sequence', 'check (sequence >0)', "sequence must be >=0 !")]
