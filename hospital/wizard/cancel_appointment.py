31e8c9b708514cd346512005a32a22ec7b63086e
ellouzeibtissem <ibtissem.ellouze@enetcom.u-sfax.tn>
2024-06-24 12:01:27 +0100

    How To Add Buttons In Odoo Tree View || Set Icons For Buttons in Odoo

--- hospital/wizard/cancel_appointment.py
+++ hospital/wizard/cancel_appointment.py
@@ -15,11 +15,13 @@ class CancelAppointmentWizard(models.TransientModel):
             res['appointment_id'] = self.env.context.get('active_id')
         return res
 
-    appointment_id = fields.Many2one('hospital.appointment', string="Appointment", domain=[('state', '=', 'draft'), ('priority', 'in', ('0', '1', False))])
+    appointment_id = fields.Many2one('hospital.appointment', string="Appointment",
+                                     domain=[('state', '=', 'draft'), ('priority', 'in', ('0', '1', False))])
     reason = fields.Text(string="Reason", default='Test_Reason')
     date_cancel = fields.Date(string="cancellation Date")
 
     def action_cancel(self):
         if self.appointment_id.booking_date == fields.Date.today():
             raise ValidationError(_("Sorry , Cancellation is not allowed on the same day of booking !"))
+        self.appointment_id.state = 'cancel'
         return
