0c8dfd5ac4900e94421c7d98cffc3dbf8d7ee877
ellouzeibtissem <ibtissem.ellouze@enetcom.u-sfax.tn>
2024-07-11 17:29:46 +0100

    Calendar View in Odoo

--- hospital/models/appointment.py
+++ hospital/models/appointment.py
@@ -14,7 +14,7 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string=' Booking Date', default=fields.Date.context_today)
-    ref = fields.Char(string='Reference', help ="Reference from patient record")
+    ref = fields.Char(string='Reference', help="Reference from patient record")
     prescription = fields.Html(string='prescription')
     priority = fields.Selection([('0', 'Normal'), ('1', 'Low'), ('2', 'High'), ('3', 'very_High')], string='Priority')
     state = fields.Selection(
@@ -23,12 +23,14 @@ class HospitalAppointment(models.Model):
     doctor_id = fields.Many2one('res.users', string='Doctor', tracking=True)
     pharmacy_line_ids = fields.One2many('appointment.pharmacy.lines', 'appointment_id', string="Pharmacy Lines")
     hide_sales_prices = fields.Boolean(string=" Hide Sales Prices")
-
+    # progress = fields.Integer(string="progress", compute='_compute_progress')
+    duration = fields.Float(string="Duration")
 
     def unlink(self):
         if self.state != 'draft':
             raise ValidationError(_("You can delete only appointment in 'Draft' status"))
         return super(HospitalAppointment, self).unlink()
+
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
@@ -60,6 +62,19 @@ class HospitalAppointment(models.Model):
         for rec in self:
             rec.state = 'draft'
 
+    # @api.depends('state')
+    # def _compute_progress(self):
+    #     for rec in self:
+    #         if rec.state == 'draft':
+    #             progress = 25
+    #         elif rec.state == 'in_consultation':
+    #             progress = 50
+    #         elif rec.state == 'done':
+    #             progress = 100
+    #         else:
+    #             progress = 0
+    #             rec.progress = progress
+    #
 
 class AppointmentPharmacyLines(models.Model):
     _name = "appointment.pharmacy.lines"
